e2b7de46b18649eb071f993f4f3ed98fe99f8eb6
Mohit Mehndiratta <mkm321.mm@gmail.com>
2018-07-26 01:34:19 +0530

    Update Account_Department.cs

--- Account_Department.cs
+++ Account_Department.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
+    //Account Department class inherits Employee Class
     class Account_Department:Employee
     {
         private string qualification;
+        //passing employee details in base (super class)
         public Account_Department(string qual, int id, string name, string desn) : base(id, name, desn)
         {
             qualification = qual;
         }
+        //Getter method to get employee's qualification
         public string getQual()
         {
             return qualification;
